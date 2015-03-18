8c94b63f43a0e5cb18c15affd0667927dacde9ef
brianecker <briancecker@gmail.com>
2015-03-18 14:26:32 -0700

    Mouse settings on login.

---
+++ mouse_settings.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+xinput --set-prop "Logitech USB Gaming Mouse" "Device Accel Velocity Scaling" 1
+xinput --set-prop "Logitech USB Gaming Mouse" "Device Accel Constant Deceleration" 2.5
+
